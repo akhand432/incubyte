cd7b30ffde1bc39c41b689b62f55098474435d6d
akhand432 <akhandpratap432@gmail.com>
2024-02-17 12:55:56 +0530

    Incubyte Technical assesement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'assesement';
+
+  values: Array<string> = ["", "4", "3,4", "5"]
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    this.addStringNumbers(this.values);
+  }
+
+  addStringNumbers(numbers: Array<string>) {
+
+  }
+
 }
