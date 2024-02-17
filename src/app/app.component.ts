2b57f850102f71555630b53a9a0eadf4c775ce84
akhand432 <akhandpratap432@gmail.com>
2024-02-17 13:20:42 +0530

    Added the method to add string number

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { isEmpty } from 'rxjs';
 import { __values } from 'tslib';
 
 @Component({
@@ -9,7 +10,7 @@ import { __values } from 'tslib';
 export class AppComponent implements OnInit {
   title = 'assesement';
 
-  values: Array<string> = ["", "4", "3,4", "5"]
+  values: Array<string> = ["", "4", "4", "5"]
 
   constructor() {
 
@@ -20,6 +21,18 @@ export class AppComponent implements OnInit {
   }
 
   addStringNumbers(numbers: Array<string>) {
+    let sumNumbers = 0;
+    if(Array.isArray(this.values)) {
+      this.values.map((value, index) => {
+        if(value != "" || value != undefined) {
+          sumNumbers += parseInt(value);
+          console.log(value);
+        }
+      })
+    }else {
+      console.log("The string of numbers is not of type array of strings");
+    }
+    console.log(sumNumbers);
 
   }
 
